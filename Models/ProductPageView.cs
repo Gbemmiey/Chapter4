328ead4e7ffc351fec7e0ede92eebc31410e7c6c
Oluwagbemileke Femi Oyeyoade <gbemfemalabi@gmail.com>
2023-06-20 11:05:15 +0100

    Add project files.

---
+++ Models/ProductPageView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeriLogging.Models
+{
+    public class ProductPageView
+    {
+        public int ProductId { get; set; }
+        public string? PageTitle { get; set; }
+        public string? SiteSection { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Serilog;
+using Serilog.Core;
+using SeriLogging.Models;
+
+using Logger log = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+Log.Logger = log;
+Log.Information("The global logger has been configured");
+
+Log.Warning("Danger, Serolog Danger");
+Log.Error("This is an error");
+Log.Fatal("Fatal Problem");
+
+
+ProductPageView pageView = new()
+{
+    PageTitle = "Chai",
+    SiteSection = "Beverages",
+    ProductId = 1
+};
+
+Log.Information("{@PageView} occured at {Viewed}", pageView,
+    DateTimeOffset.UtcNow);
+
+Log.CloseAndFlush();
